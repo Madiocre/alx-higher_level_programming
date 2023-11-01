0a1ba9b85d8acb470da11b73ba4044183d8a007d
Madiocre <wolvirex@gmail.com>
2023-10-31 18:26:22 -0700

    multiple nums

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3                                                                                                                                                                           
+#!/usr/bin/python3
 letters = [chr(i) for i in range(97, 123) if chr(i) not in ['q', 'e']]
 print("{}".format(''.join(letters)), end="")
