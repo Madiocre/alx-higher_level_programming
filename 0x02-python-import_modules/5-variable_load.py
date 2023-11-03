fca9c5d0fc05b4be03c5fc61916dfd0a166e4f61
Madiocre <wolvirex@gmail.com>
2023-11-03 07:36:14 -0700

    fix attempt'

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import importlib.util
+from importlib import util
 import sys
 if __name__ == "__main__":
-    spec = importlib.util.spec_from_file_location("variable_module", "./variable_load_5.py")
-    variable_module = importlib.util.module_from_spec(spec)
+    spec =util.spec_from_file_location("variable_module", "./variable_load_5.py")
+    variable_module = util.module_from_spec(spec)
     spec.loader.exec_module(variable_module)
     a = variable_module.a
     print(a)
