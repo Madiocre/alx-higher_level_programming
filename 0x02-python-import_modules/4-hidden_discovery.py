d483e4c1aa9b8d0969da20ccdd87bceb1f5f0329
Madiocre <wolvirex@gmail.com>
2023-11-02 18:01:31 -0700

    pycode fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,8 @@
 import importlib.util
 import sys
 if __name__ == "__main__":
-    spec = importlib.util.spec_from_file_location("hidden_module", "./hidden_4.pyc")
+    spec = importlib.util.spec_from_file_location("hidden_module",\
+                                                  "./hidden_4.pyc")
     hidden_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(hidden_module)
     names = dir(hidden_module)
