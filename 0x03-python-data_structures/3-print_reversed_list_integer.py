c66c179a392350a78011c35e8e00dc186fbd1423
Madiocre <wolvirex@gmail.com>
2023-11-06 16:56:12 -0800

    fixed reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for item in reversed(range(len(my_list))):
-        print("{:d}".format(my_list[item]))
+    for item in reversed(my_list):
+        print("{:d}".format(item))
