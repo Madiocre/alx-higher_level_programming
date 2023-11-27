b177ae700e31835a89f8d382e36d46e6ed5b9988
Madiocre <wolvirex@gmail.com>
2023-11-27 14:47:24 -0800

    mod doc

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,19 +5,18 @@ This module provides a Square class that represents a square.
 The Square class has a private attribute '__size' that represents the size of the square.
 It has methods to get and set the size of the square.
 """
+
 class Square:
     """
-   A class representing a square.
-
-   Attributes:
-       __size (int): The size of the square.
-   """
-   def __init__(self, size):
-       """
-       The constructor for Square class.
-
-       Args:
-           size (int): The size of the square.
-       """
-       self.__size = size
+    A class representing a square.                                                                                                                                                   
+    Attributes:
+    __size (int): The size of the square.
+    """
+    def __init__(self, size):
+        """                                                                                                                                                                        
+        The constructor for Square class.
+        Args:
+        size (int): The size of the square.
+        """
+        self.__size = size
 
