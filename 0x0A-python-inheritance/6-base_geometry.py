81450f2b06db0307dab58bc840c705dca3f62393
Madiocre <wolvirex@gmail.com>
2023-12-04 17:41:26 -0800

    0x0A 6-7 with test

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module Disc
+"""
+
+
+class BaseGeometry:
+    """
+    Class to raise Exception
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
