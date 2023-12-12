54bd97fa407c05da1244a4184622d7dc72d94700
madiocre <wolvirex@gmail.com>
2023-12-12 02:40:27 +0200

    0x0C pycodestyle

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,9 +1,6 @@
 import unittest
 from models.base import Base
 from models.square import Square
-from random import randrange
-from contextlib import redirect_stdout
-import io
 
 
 class TestSquare(unittest.TestCase):
